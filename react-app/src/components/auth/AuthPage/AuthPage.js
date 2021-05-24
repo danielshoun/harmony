256fdb4848372617f5f5d9bf1ce96d8da8d6394f
Danny Shoun <danielshoun+servicesgithub@pm.me>
2021-05-24 16:09:57 -0400

    Working on server directory CSS.

--- react-app/src/components/auth/AuthPage/AuthPage.js
+++ react-app/src/components/auth/AuthPage/AuthPage.js
@@ -6,14 +6,14 @@ const AuthPage = () => {
 
   useEffect(() => {
     const tempServers = [
-      {id: 1, name: 'Test Server 2', ownerName: 'Demo User', imageUrl: null},
-      {id: 2, name: 'Test Server 3', ownerName: 'Demo User', imageUrl: null},
-      {id: 3, name: 'Test Server 1', ownerName: 'Demo User', imageUrl: null},
-      {id: 4, name: 'Test Server 4', ownerName: 'Demo User', imageUrl: null},
-      {id: 5, name: 'Test Server 5', ownerName: 'Demo User', imageUrl: null},
-      {id: 6, name: 'Test Server 6', ownerName: 'Demo User', imageUrl: null},
-      {id: 7, name: 'Test Server 7', ownerName: 'Demo User', imageUrl: null},
-      {id: 8, name: 'Test Server 8', ownerName: 'Demo User', imageUrl: null},
+      {id: 1, name: 'Test Server 2', ownerName: 'Demo User', userCount: 100, imageUrl: null},
+      {id: 2, name: 'Test Server 3', ownerName: 'Demo User', userCount: 50, imageUrl: null},
+      {id: 3, name: 'Test Server 1', ownerName: 'Demo User', userCount: 200, imageUrl: null},
+      {id: 4, name: 'Test Server 4', ownerName: 'Demo User', userCount: 300, imageUrl: null},
+      {id: 5, name: 'Test Server 5', ownerName: 'Demo User', userCount: 40, imageUrl: null},
+      {id: 6, name: 'Test Server 6', ownerName: 'Demo User', userCount: 1, imageUrl: null},
+      {id: 7, name: 'Test Server 7', ownerName: 'Demo User', userCount: 10, imageUrl: null},
+      {id: 8, name: 'Test Server 8', ownerName: 'Demo User', userCount: 25, imageUrl: null},
     ]
 
     setServers(tempServers)
@@ -43,6 +43,8 @@ const AuthPage = () => {
                 {server.imageUrl ? <img className="server-image" src={server.imageUrl} alt={server.name}/> : <div className="server-image">{server.name[0]}</div>}
                 <div className="server-name">{server.name}</div>
                 <div className="server-owner">{server.ownerName}</div>
+                <div className="server-count">{server.userCount} users</div>
+                <button className="button-primary join-button">Join</button>
               </div>
           )
         })}
---
+++ react-app/src/components/auth/SideBar/index.js
@@ -0,0 +1,10 @@
+import React, {useEffect, useState} from "react";
+
+const SideBar = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default SideBar;
