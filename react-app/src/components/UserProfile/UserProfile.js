c680b43155a139b407bfcc7bb62d7fe64989a8f3
Kevin Zheng <jiezheng2020@gmail.com>
2021-05-25 16:12:18 -0500

    modal barebone done

--- react-app/src/components/UserProfile/UserProfile.js
+++ react-app/src/components/UserProfile/UserProfile.js
@@ -2,10 +2,16 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/session";
+import UsernameModal from "./UserModals/UsernameModal";
 import "./UserProfile.css";
+import "./UserModals/Modal.css";
 
 function User() {
   const [user, setUser] = useState({});
+  const [showNameModal, setNameModal] = useState(false);
+  const [showEmailModal, setEmailModal] = useState(false);
+  const [showPicModal, setPicModal] = useState(false);
+
   const dispatch = useDispatch();
   const { userId } = useParams();
 
@@ -37,61 +43,78 @@ function User() {
   const editImage = async () => {
     //Logic Needed
   };
+  console.log(showNameModal);
 
   return (
-    <div className="account-profile-container">
-      <h1>My Account</h1>
-      <div className="account-details-box">
-        <div className="user-profile-header">
-          <div className="user-profile-icon">
-            {user.pictureUrl ? (
-              <img
-                className="user-prof-pic"
-                src={user.pictureUrl}
-                alt={user.username}
-              />
-            ) : (
-              <div className="user-profile-icon">{user.username[0]}</div>
-            )}
-            <div class="overlay">
-              <div
-                onClick={() => {
-                  editImage();
-                }}
-              >
-                CHANGE
+    <>
+      <div className="account-profile-container">
+        <h1>My Account</h1>
+        <div className="account-details-box">
+          <div className="user-profile-header">
+            <div className="user-profile-icon">
+              {user.pictureUrl ? (
+                <img
+                  className="user-prof-pic"
+                  src={user.pictureUrl}
+                  alt={user.username}
+                />
+              ) : (
+                <div className="user-profile-icon">{user.username[0]}</div>
+              )}
+              <div class="overlay">
+                <div
+                  onClick={() => {
+                    editImage();
+                  }}
+                >
+                  CHANGE
+                </div>
               </div>
             </div>
+            <div className="user-profile-name">{user.username}</div>
+            <div className="user-profile-name-id">#{user.id}</div>
           </div>
-          <div className="user-profile-name">{user.username}</div>
-          <div className="user-profile-name-id">#{user.id}</div>
-        </div>
-        <div className="user-profile-details">
-          <div className="user-edit-bar">
-            <div>
-              <label className="user-labels">USERNAME</label>
-              <div className="user-info">{user.username}</div>
+          <div className="user-profile-details">
+            <div className="user-edit-bar">
+              <div>
+                <label className="user-labels">USERNAME</label>
+                <div className="user-info">{user.username}</div>
+              </div>
+              <button
+                onClick={() => setNameModal(true)}
+                className="user-edit-btn"
+              >
+                Edit
+              </button>
             </div>
-            <button onClick={() => editUserName()} className="user-edit-btn">
-              Edit
-            </button>
-          </div>
-          <div className="user-edit-bar">
-            <div>
-              <label className="user-labels">EMAIL</label>
-              <div className="user-info">{user.email}</div>
+            <div className="user-edit-bar">
+              <div>
+                <label className="user-labels">EMAIL</label>
+                <div className="user-info">{user.email}</div>
+              </div>
+              <button onClick={() => editEmail()} className="user-edit-btn">
+                Edit
+              </button>
             </div>
-            <button onClick={() => editEmail()} className="user-edit-btn">
-              Edit
-            </button>
-          </div>
 
-          <button onClick={() => logOut()} className="account-logout-btn">
-            Logout
-          </button>
+            <button onClick={() => logOut()} className="account-logout-btn">
+              Logout
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      {showNameModal && (
+        <div className="modal">
+          <div
+            className="modal-background"
+            onClick={() => setNameModal(false)}
+          />
+          <div className="modal-content">
+            <UsernameModal />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 export default User;
