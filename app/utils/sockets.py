cb8596b814f8433a32798f375277ad7506e17637
Elias Eichen <eliprof@live.com>
2021-05-25 15:53:57 -0500

    working messages

--- app/utils/sockets.py
+++ app/utils/sockets.py
@@ -1,6 +1,8 @@
 from flask_socketio import SocketIO, join_room, leave_room, send
 import os
 
+from app.models import ChannelMessage, db
+
 if os.environ.get('FLASK_ENV') == 'production':
     origins = [
         'placeholder'
@@ -16,6 +18,20 @@ def on_join(data):
     room = data['channel_id']
     join_room(room)
     send('test join')
+
+
+@socketio.on('chat')
+def channel_chat(data):
+    # print(data['message'])
+    new_message = ChannelMessage(
+        sender_id=data['sender_id'],
+        channel_id=data['channel_id'],
+        body=data['body']
+    )
+    db.session.add(new_message)
+    db.session.commit()
+
+    send(new_message.to_dict(), to=str(data['channel_id']))
 
 
 @socketio.on('connect')
