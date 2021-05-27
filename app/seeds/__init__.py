78fd5bf316409a1d4660c8e33551c0018bf8fb94
Elias Eichen <eliprof@live.com>
2021-05-27 18:02:08 -0500

    added channel messages seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,23 +2,30 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .servers import seed_servers, undo_servers
 from .channels import seed_channels, undo_channels
+from .channel_messages import seed_channel_messages, undo_channel_messages
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
 
 # Creates the `flask seed all` command
+
+
 @seed_commands.command('all')
 def seed():
     seed_users()
     seed_servers()
     seed_channels()
+    seed_channel_messages()
     # Add other seed functions here
 
 # Creates the `flask seed undo` command
+
+
 @seed_commands.command('undo')
 def undo():
     undo_users()
     undo_servers()
     undo_channels()
+    undo_channel_messages()
     # Add other undo functions here
---
+++ app/seeds/channel_messages.py
@@ -0,0 +1,36 @@
+from app.models import db, User, Server, Channel, ChannelMessage
+from faker import Faker
+from random import randint
+from datetime import datetime, timedelta
+
+
+fake = Faker()
+
+
+def seed_channel_messages():
+    channel_messages = []
+    channels = Channel.query.all()
+
+    time = datetime.utcnow()
+    time_change = timedelta(minutes=randint(1, 5))
+
+    for i in range(1, len(channels)):
+        for j in range(1, 57):
+            channel_message = ChannelMessage(
+                body=fake.sentence(),
+                channel_id=i,
+                sender_id=randint(1, 30),
+                created_at=time + time_change
+            )
+            time_change += timedelta(minutes=randint(1, 5))
+            channel_messages.append(channel_message)
+
+        time_change = timedelta(milliseconds=randint(1, 5))
+
+    db.session.add_all(channel_messages)
+    db.session.commit()
+
+
+def undo_channel_messages():
+    db.session.execute('TRUNCATE channel_messages RESTART IDENTITY CASCADE;')
+    db.session.commit()
