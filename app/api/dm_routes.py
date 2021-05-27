56e0bfde7bc3e6f975b83a9d2d544e5ee040f261
Elias Eichen <eliprof@live.com>
2021-05-27 11:32:26 -0500

    fixed new dms

--- app/api/dm_routes.py
+++ app/api/dm_routes.py
@@ -18,6 +18,33 @@ def get_conversations():
     return jsonify([conversation.to_dict() for conversation in conversations])
 
 
+@dm_routes.route('/', methods=['POST'])
+@login_required
+def create_conversation():
+    data = request.json
+    other_user_id = data['memberId']
+
+    conversation = Conversation.query.filter(or_(
+        and_(Conversation.user_1_id == current_user.id,
+             Conversation.user_2_id == other_user_id),
+        and_(Conversation.user_2_id == current_user.id,
+             Conversation.user_1_id == other_user_id))).all()
+
+    print('convo5', conversation)
+    if(len(conversation) > 0):
+        return {'message': 'already exists'}
+
+    conversation = Conversation(
+        user_1_id=current_user.id,
+        user_2_id=data['memberId']
+    )
+
+    db.session.add(conversation)
+    db.session.commit()
+
+    return {'message': 'created new conversaiton'}
+
+
 @dm_routes.route('/<int:other_user_id>')
 @login_required
 def get_private_messages(other_user_id):
