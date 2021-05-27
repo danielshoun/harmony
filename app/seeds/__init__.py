e377dea08b98f07958b4a66168eec41b9a28d747
Elias Eichen <eliprof@live.com>
2021-05-27 17:25:16 -0500

    channels seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .servers import seed_servers, undo_servers
+from .channels import seed_channels, undo_channels
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -11,6 +12,7 @@ seed_commands = AppGroup('seed')
 def seed():
     seed_users()
     seed_servers()
+    seed_channels()
     # Add other seed functions here
 
 # Creates the `flask seed undo` command
@@ -18,4 +20,5 @@ def seed():
 def undo():
     undo_users()
     undo_servers()
+    undo_channels()
     # Add other undo functions here
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,6 +5,8 @@ from faker import Faker
 fake = Faker()
 
 # Adds a demo user, you can add other users here if you want
+
+
 def seed_users():
 
     demo = User(username='Demo', email='demo@aa.io',
@@ -13,8 +15,12 @@ def seed_users():
     demo2 = User(username='Demo 2', email='demo2@aa.io', password='password')
 
     seed_users = [demo, demo2]
-    for i in range(1, 20):
-        seed_users.append(User(username=fake.user_name(), email=fake.email(), password='password'))
+    for i in range(1, 30):
+        seed_users.append(User(
+            username=fake.user_name(), 
+            email=fake.email(),
+            password='password'
+            ))
 
     db.session.add_all(seed_users)
 
@@ -24,6 +30,8 @@ def seed_users():
 # SQLAlchemy doesn't have a built in function to do this
 # TRUNCATE Removes all the data from the table, and resets
 # the auto incrementing primary key
+
+
 def undo_users():
     db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
     db.session.commit()
